7610da9cc2ca31b05ce09276c1f383ca1bc882c1
wushengzhu <ws_zhu@foxmail.com>
2023-05-17 18:17:07 +0800

    feat: add hooks

---
+++ learn-platform-pc/src/hooks/useRequest.ts
@@ -0,0 +1,53 @@
+import { useCallback, useState } from "react";
+import useMount from "./useMount";
+
+interface IOptions {
+  params: Record<string, string>;
+  manual?: boolean;
+  onSuccess?: (res: unknown) => void;
+  onError?: (err: unknown) => void;
+}
+/**
+ * 实现组件初始化，发送请求获取数据
+ * 手动触发请求
+ * @param service
+ * @param params
+ * @returns
+ */
+const useRequest = (
+  service: (params: Record<string, string>) => Promise<unknown>,
+  options: IOptions
+) => {
+  const [data, setData] = useState<unknown>();
+  const [loading, setLoading] = useState<unknown>();
+
+  const init = useCallback(
+    (curParams: Record<string, string>) => {
+      setLoading(true);
+      return service(curParams)
+        .then((res) => {
+          setData(res);
+          setLoading(false);
+          options.onSuccess && options.onSuccess(res);
+        })
+        .catch((error) => {
+          setLoading(false);
+          options.onError && options.onError(error);
+        });
+    },
+    [service]
+  );
+  useMount(() => {
+    if (!options.manual) {
+      init(options.params);
+    }
+  });
+
+  const run = (runParams: Record<string, string>) => {
+    init(runParams);
+  };
+
+  return [loading, data, run];
+};
+
+export default useRequest;
