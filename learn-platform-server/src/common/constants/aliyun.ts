b744a70a78fcab45db5d3f1b592defdfdefb26d8
wushengzhu <ws_zhu@foxmail.com>
2023-05-20 14:53:17 +0800

    feat: add constant

---
+++ learn-platform-server/src/common/constants/aliyun.ts
@@ -0,0 +1,5 @@
+export const SIGN_NAME = '兴趣班学习平台'
+export const TEMPLATECODE = ''
+export const ACCESS_KEY_ID = 'LTAI5tHx2pEgFVS5LTcWwchb'
+export const ACCESS_KEY_SECRET = '3iPhJVIeswujZppq4Yzd8u5JMNX6G5'
+export const BUCKET = 'learn-platform-assets'
