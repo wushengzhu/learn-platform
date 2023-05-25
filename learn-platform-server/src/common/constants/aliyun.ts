4f9a87b9c8be7bd78cfc92e95d87721cf82f6604
Sapper <59349073+wushengzhu@users.noreply.github.com>
2023-05-25 23:38:13 +0800

    Update aliyun.ts

--- learn-platform-server/src/common/constants/aliyun.ts
+++ learn-platform-server/src/common/constants/aliyun.ts
@@ -4,11 +4,5 @@ export const TEMPLATE_CODE = 'SMS_460816160'
 // 这个已经被禁用，可以自己配一下个人阿里云
 // export const ACCESS_KEY_ID = 'LTAI5tHx2pEgFVS5LTcWwchb'
 // export const ACCESS_KEY_SECRET = '3iPhJVIeswujZppq4Yzd8u5JMNX6G5'
-// 访问oos的新key
-export const ACCESS_KEY_ID = 'LTAI5tMGsVn9Fa8LiZmihhas'
-export const ACCESS_KEY_SECRET = 'mmJnuiNySaY8gyx528PFb8oxPECsDq'
 
-// 访问api的key
-// export const ACCESS_KEY_ID = 'LTAI5tBsYY54m9s4UNLMDbfc'
-// export const ACCESS_KEY_SECRET = '2Tp6eXJx8Wr5QOnfDAlnQKVLNP9uxz'
 export const BUCKET = 'learn-platform-assets'
