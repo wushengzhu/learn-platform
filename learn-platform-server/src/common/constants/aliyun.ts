9932121a08e189ca5eaf2e9a4e7a46312db603ce
Sapper <59349073+wushengzhu@users.noreply.github.com>
2023-05-25 23:38:48 +0800

    Update aliyun.ts

--- learn-platform-server/src/common/constants/aliyun.ts
+++ learn-platform-server/src/common/constants/aliyun.ts
@@ -2,7 +2,7 @@
 export const SIGN_NAME = '兴趣学习班平台短信验证码'
 export const TEMPLATE_CODE = 'SMS_460816160'
 // 这个已经被禁用，可以自己配一下个人阿里云
-// export const ACCESS_KEY_ID = 'LTAI5tHx2pEgFVS5LTcWwchb'
-// export const ACCESS_KEY_SECRET = '3iPhJVIeswujZppq4Yzd8u5JMNX6G5'
+export const ACCESS_KEY_ID = 'LTAI5tHx2pEgFVS5LTcWwchb'
+export const ACCESS_KEY_SECRET = '3iPhJVIeswujZppq4Yzd8u5JMNX6G5'
 
 export const BUCKET = 'learn-platform-assets'
