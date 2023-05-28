3fdd426251d761cd0bbd5dff82e3c92b83a2ec2d
wushengzhu <ws_zhu@foxmail.com>
2023-05-28 09:31:58 +0800

    fix：add less global

--- learn-platform-mobile/src/containers/Login/index.tsx
+++ learn-platform-mobile/src/containers/Login/index.tsx
@@ -4,13 +4,14 @@ import {
   Input,
   Button,
 } from 'antd-mobile';
-import './index.module.less';
 import { EyeInvisibleOutline, EyeOutline } from 'antd-mobile-icons'
+import styles from './index.module.less';
 
-export default () => {
+export default (props: any) => {
   const onFinish = (values: any) => {
 
   };
+
   const [visible, setVisible] = useState(false)
   const [rules] = useState({
     username: [
@@ -29,16 +30,17 @@ export default () => {
 
   return (
     <>
-      <div className="login-container">
+      <div className={styles['login-container']}>
         <div className="w-30">
           <img src={"https://github.githubassets.com/images/modules/logos_page/Octocat.png"} />
         </div>
         <Form
           layout="horizontal"
+          className={styles['form-container']}
           onFinish={onFinish}
           footer={(
             <div className="flex justify-center">
-              <Button block shape='rounded' size="middle" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)", border: 0, color: 'white' }}>
+              <Button block onClick={() => { props.history.push('/register') }} shape='rounded' size="middle" style={{ backgroundColor: "rgba(0, 0, 0, 0.6)", border: 0, color: 'white' }}>
                 注 册
               </Button>
               <Button block shape='rounded' type="submit" color="primary" size="middle" className="left-2">
@@ -47,16 +49,16 @@ export default () => {
             </div>
           )}
         >
-          <Form.Header><span className="form-title">LearnPlatform</span></Form.Header>
+          <Form.Header><span className={styles['form-title']}>LearnPlatform</span></Form.Header>
           <Form.Item
             name="account"
             rules={rules.username}
-            className="form-item"
+            className={styles['form-item']}
           >
             <Input placeholder="请输入账号" />
           </Form.Item>
-          <Form.Item name="password" rules={rules.password} className="form-item mt-4 mb-4" extra={
-            <div className='eve'>
+          <Form.Item name="password" rules={rules.password} className={styles['form-item']} extra={
+            <div className={styles.eye}>
               {!visible ? (
                 <EyeInvisibleOutline onClick={() => setVisible(true)} />
               ) : (
